c3cee962a7a90f0c6cd88c87021b5dce310d0951
David LeGare <excaliburhissheath@gmail.com>
2017-09-22 21:41:59 -0500

    Make the cube rotate

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use polygon::gl::GlRender;
 use polygon::light::*;
 use polygon::math::*;
 use polygon::mesh_instance::*;
+use std::time::*;
 use winit::*;
 
 mod collada;
@@ -19,7 +20,10 @@ mod collada;
 fn main() {
     // Open a window.
     let mut events_loop = EventsLoop::new();
-    let window = Window::new(&events_loop).expect("Failed to open window");
+    let window = WindowBuilder::new()
+        .with_dimensions(800, 800)
+        .build(&events_loop)
+        .expect("Failed to open window");
 
     // Create the OpenGL context and the renderer.
     let context = window.create_context().expect("Failed to create GL context");
@@ -60,6 +64,8 @@ fn main() {
     renderer.register_camera(camera);
 
     let mut loop_active = true;
+    let frame_time = Duration::from_secs(1) / 60;
+    let mut next_loop_time = Instant::now() + frame_time;
     while loop_active {
         events_loop.poll_events(|event| {
             match event {
@@ -72,7 +78,19 @@ fn main() {
         });
         if !loop_active { break; }
 
+        {
+            let mesh_anchor = renderer.get_anchor_mut(mesh_anchor_id).unwrap();
+            let orientation = mesh_anchor.orientation();
+            let change = Orientation::from_eulers(TAU / 4.0 / 60.0, TAU / 6.0 / 60.0, TAU / 8.0 / 60.0);
+            mesh_anchor.set_orientation(orientation + change);
+        }
+
         // Render the mesh.
         renderer.draw();
+
+        // Wait for the next frame.
+        // TODO: Wait more efficiently by sleeping the thread.
+        while Instant::now() < next_loop_time {}
+        next_loop_time += frame_time;
     }
 }
